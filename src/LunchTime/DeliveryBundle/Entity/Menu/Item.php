8686923d6a99fca757c19ccaec585f3068a00926
Dziamid <dziamid@gmail.com>
2012-02-25 16:25:49 +0300

    added Menu/Item entity

---
+++ src/LunchTime/DeliveryBundle/Entity/Menu/Item.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace LunchTime\DeliveryBundle\Entity\Menu;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * LunchTime\DeliveryBundle\Entity\Menu\Item
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Item
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $title
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+}
