29c428d8f30d04461a127ef4fd3a6f1597e8ff01
Dziamid <dziamid@gmail.com>
2012-02-27 10:42:56 +0300

    added ember-data

--- src/LunchTime/DeliveryBundle/Resources/public/js/app.js
+++ src/LunchTime/DeliveryBundle/Resources/public/js/app.js
@@ -1,7 +1,20 @@
 var App = Em.Application.create();
+App.store = DS.Store.create({
+    adapter: DS.RESTAdapter.create({ bulkCommit: false })
+});
+
+App.Menu = DS.Model.extend({
+    dueDate: DS.attr('date'),
+    items: DS.hasMany('App.MenuItem')
+});
+
+App.MenuItem = DS.Model.extend({
+    title: DS.attr('string')
+});
+
+var menus = App.store.findAll(App.Menu);
+var items = App.store.findAll(App.MenuItem);
 
 App.MyView = Em.View.extend({
-  mouseDown: function() {
-    window.alert("hello world!");
-  }
+  menus: menus
 });
