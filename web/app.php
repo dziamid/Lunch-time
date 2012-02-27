2c86b2226479e67e1e99e1bc75d18e2f3bea0139
Dziamid <dziamid@gmail.com>
2012-02-27 20:36:04 +0300

    turned off class cache in prod

--- web/app.php
+++ web/app.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once __DIR__.'/../app/bootstrap.php.cache';
+// require_once __DIR__.'/../app/bootstrap.php.cache';
+require_once __DIR__.'/../vendor/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once __DIR__.'/../app/autoload.php';
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
 $kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+// $kernel->loadClassCache();
 $kernel->handle(Request::createFromGlobals())->send();
