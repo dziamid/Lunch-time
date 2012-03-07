cae0b0abc7dced89107b210f812cb442878e5d04
Dziamid <dziamid@gmail.com>
2012-03-07 16:34:02 +0300

    added menu item view

--- src/LunchTime/DeliveryBundle/Resources/public/js/view/active-menu-view.js
+++ src/LunchTime/DeliveryBundle/Resources/public/js/view/active-menu-view.js
@@ -1,3 +1,13 @@
 App.ActiveMenuView = Em.View.extend({
      menuBinding: 'App.MenuController.active'
 });
+
+App.MenuItemView = Em.View.extend({
+    click: function () {
+        console.log('Item clicked');
+        var orderItem = App.OrderItem.create({
+            title: this.item.get('title')
+        });
+        App.getPath('OrderController.active.items').pushObject(orderItem);
+    }
+});
