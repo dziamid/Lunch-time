bddeada954d4b4a97c1779b8ceb679d3c3a3bcdb
Dziamid <dziamid@gmail.com>
2012-07-02 16:19:09 +0300

    added foruserbundle

--- src/LunchTime/DeliveryBundle/Controller/DefaultController.php
+++ src/LunchTime/DeliveryBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace LunchTime\DeliveryBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use JMS\SecurityExtraBundle\Annotation\Secure;
 
 class DefaultController extends Controller
 {
